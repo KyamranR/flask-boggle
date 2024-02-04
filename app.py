22eb069c55afb0e97808b6816d1eb0089411ddba
Kyamran Rizayev <kyamranr@gmail.com>
2024-02-03 22:57:37 -0500

    added check-word route

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ boggle_game = Boggle()
 def homepage():
     """Home Page"""
     board = boggle_game.make_board()
+    session['board'] = board
 
     return render_template('index.html', board=board)
     
+
+@app.route('/check-word')
+def check_word():
+    """Checking the words"""
+    word = request.args['word']
+    board = session['board']
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
